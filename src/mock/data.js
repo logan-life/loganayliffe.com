a98566cbd46e3f8a095e4f21463a7636cf36e882
Logan Ayliffe <lmayliffe@gmail.com>
2023-01-11 16:09:51 +0100

    remove old project and minor langauge edits

--- src/mock/data.js
+++ src/mock/data.js
@@ -42,7 +42,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'excessive_alcohol_use_project_image.png',
     title: 'Can Twitter predict US county-level excessive alcohol consumption? (2021)',
-    info: 'Collaborated in a team of 3 to implement published ridge regression ML model which uses NLP derived features from dense, high dimensional Twitter data to predict excessive alcohol consumption.',
+    info: 'Implement published ridge regression ML model which uses NLP derived features from dense, high dimensional Twitter data to predict excessive alcohol consumption.',
     info2:
       'Experimented with various methods of dimensionality reduction to improve model performance. Designed and implement feed-forward neural network to replace prior model yielding significant improvement in both Pearson R and mean absolute error.',
     url: '',
@@ -52,7 +52,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'twitter_covid_cases_project_image.png',
     title: 'Predicting COVID case rate change by US state using Big Twitter Data (2021)',
-    info: 'Collaborated in a team of 3 to extract, transform, and load large Twitter datasets for use in multiple ML models using Academic Research Twitter API endpoints.',
+    info: 'Extract, transform, and load large Twitter datasets for use in multiple ML models using Academic Research Twitter API endpoints.',
     info2:
       'Designed and constructed ML model using TF-IDF scores for ngram usage in COVID related tweets by US state. Experimented with baseline extensions and improved model performance to accurately predict percent change in COVID-19 case rates based on ngram scores.',
     url: '',
@@ -62,7 +62,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'soundtrack_surfer_project_image.png',
     title: 'SoundtrackSurfer (2021)',
-    info: 'Collaborate in a team of 4 to create single-page application which explores film soundtracks, intelligently recommends relevant artists / films / songs, and plays embedded spotify tracks.',
+    info: 'Create single-page application which explores film soundtracks, intelligently recommends relevant artists / films / songs, and plays embedded spotify tracks.',
     info2:
       'Primary responsibilities include react frontend, advanced database query optimization, and spotify integration.',
     url: '',
@@ -72,21 +72,11 @@ export const projectsData = [
     id: nanoid(),
     img: 'pennpals_project_image.png',
     title: 'PennPals (2020)',
-    info: 'Collaborate in a team of 3 to design and create a WhatsApp clone using MERN stack and Twilio Conversations API.',
+    info: 'Design and create a WhatsApp clone using MERN stack and Twilio Conversations API.',
     info2:
       'Design and develop messaging and multiple party audio and video chat functionality built on top of the Twilio Conversations API. Primary project manager for the team using agile methodology and GitHub Projects.',
     url: '',
     repo: 'https://github.com/logan-life/penn_pals', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'professor_59x_project_image.png',
-    title: 'Professor59X (2020)',
-    info: 'Design and develop Twitter bot for University of Pennsylvania graduate student organization',
-    info2:
-      'Monitor UPenn related tweets, interact with Penn community online, post fair-use Penn images from official Penn photostreams.',
-    url: 'https://twitter.com/59xProfessor',
-    repo: 'https://github.com/logan-life/Professor_59X', // if no repo, the button will not show up
   },
 ];
 
